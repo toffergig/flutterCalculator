543971644d894d6ea871597c26000ecb959ee501
Kristoffer John Giganto <kristoffersoncool@gmail.com>
2024-02-27 14:03:26 +0800

    added calculator operations

--- lib/calculator/data/repository/calculator_repository.dart
+++ lib/calculator/data/repository/calculator_repository.dart
@@ -1,25 +1,20 @@
 class CalculatorRepository {
-  // late double _num1;
-  // late double _num2;
 
-  // CalculatorRepository() {
-  //   _num1 = 0;
-  //   _num2 = 0;
-  // }
-
-  int addition(int num1, int num2) {
-    return num1 + num2;
+  double addition(double num1, double num2){
+    return num1+num2;
   }
 
-  double subtraction(double num1, double num2) {
-    return num1 - num2;
+  double subtraction(double num1, double num2){
+    return num1-num2;
   }
 
-  double division(double num1, double num2) {
-    return num1 == 0 || num2 == 0? throw Exception() : num1 / num2;
+  double multiplication(double num1, double num2){
+    return num1*num2;
   }
 
-  double multplication(double num1, double num2) {
-    return num1 * num2;
+  double division(double num1, double num2){
+    return num1==0||num2==0? throw Exception() : num1 / num2;
   }
+
+ 
 }
--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -12,10 +12,10 @@ void main() {
         'Addition:Given two numbers, When add method is called, Then the sum is calculated correctly',
         () {
       // Given
-      int number1 = 2;
-      int number2 = 3;
+      double number1 = 2;
+      double number2 = 3;
       // When
-      int result = calculatorRepository.addition(number1, number2);
+      double result = calculatorRepository.addition(number1, number2);
       // Then
       expect(result, equals(5));
     });
@@ -24,10 +24,10 @@ void main() {
         'Addition: Given two negative numbers, When add method is called, Then the sum is calculated correctly',
         () {
       // Given
-      int number1 = -1;
-      int number2 = 1;
+      double number1 = -1;
+      double number2 = 1;
       // When
-      int result = calculatorRepository.addition(number1, number2);
+      double result = calculatorRepository.addition(number1, number2);
       // Then
       expect(result, equals(0));
     });
@@ -37,44 +37,44 @@ void main() {
     test(
         'Given Calculator class, when multiplication is called and both numbers are positive, then output positive number for multiplying positive numbers',
         () {
-      expect(calculatorRepository.multplication(2, 3), equals(6));
-      expect(calculatorRepository.multplication(3, 2), equals(6));
+      expect(calculatorRepository.multiplication(2, 3), equals(6));
+      expect(calculatorRepository.multiplication(3, 2), equals(6));
     });
 
     test(
         'Given Calculator class, when multiplication is called and both numbers are negative, then output positive number for multiplying positive numbers',
         () {
-      expect(calculatorRepository.multplication(-2, -3), equals(6));
+      expect(calculatorRepository.multiplication(-2, -3), equals(6));
     });
 
     test(
         'Given Calculator class, when multiplication is called and both are mixed signs, then output negative number for multiplying mixed signed numbers',
         () {
-      expect(calculatorRepository.multplication(2, -3), equals(-6));
-      expect(calculatorRepository.multplication(-3, 2), equals(-6));
+      expect(calculatorRepository.multiplication(2, -3), equals(-6));
+      expect(calculatorRepository.multiplication(-3, 2), equals(-6));
     });
 
     test(
         'Given Calculator class, when multiplication is called and there is a zero, then output zero',
         () {
-      expect(calculatorRepository.multplication(0, 5), equals(0));
-      expect(calculatorRepository.multplication(5, 0), equals(0));
-      expect(calculatorRepository.multplication(0, 0), equals(0));
+      expect(calculatorRepository.multiplication(0, 5), equals(0));
+      expect(calculatorRepository.multiplication(5, 0), equals(0));
+      expect(calculatorRepository.multiplication(0, 0), equals(0));
     });
 
     test(
         'Given Calculator class, when multiplication is called and there is a 1, then output same number multiplied to one',
         () {
-      expect(calculatorRepository.multplication(1, 5), equals(5));
-      expect(calculatorRepository.multplication(5, 1), equals(5));
-      expect(calculatorRepository.multplication(1, 1), equals(1));
+      expect(calculatorRepository.multiplication(1, 5), equals(5));
+      expect(calculatorRepository.multiplication(5, 1), equals(5));
+      expect(calculatorRepository.multiplication(1, 1), equals(1));
     });
 
     test(
         'Given Calculator class, when multiplication is called and there is a decimal number, then output correct product for multiplying floating-point numbers',
         () {
-      expect(calculatorRepository.multplication(2.5, 4), equals(10));
-      expect(calculatorRepository.multplication(4, 2.5), equals(10));
+      expect(calculatorRepository.multiplication(2.5, 4), equals(10));
+      expect(calculatorRepository.multiplication(4, 2.5), equals(10));
     });
   });
 
