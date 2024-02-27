173c03ea77810e2952e524dec7ccb0d514f728d1
Kristoffer John Giganto <kristoffersoncool@gmail.com>
2024-02-27 11:17:03 +0800

    updated mult desc

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -4,31 +4,27 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
    final CalculatorRepository calculatorRepository = CalculatorRepository();
   group('Multiplication', () {
-    test('Multiplying positive integers', () {
+    test('iven Calculator class, when multiplication is called and both numbers are positive, then output positive number for multiplying positive numbers', () {
       expect(calculatorRepository.multplication(2, 3), equals(6));
     });
 
-    test('Multiplying negative integers', () {
+    test('Given Calculator class, when multiplication is called and both numbers are negative, then output positive number for multiplying positive numbers', () {
       expect(calculatorRepository.multplication(-2, -3), equals(6));
     });
 
-    test('Multiplying mixed signs', () {
+    test('Given Calculator class, when multiplication is called and both are mixed signs, then output negative number for multiplying mixed signed numbers', () {
       expect(calculatorRepository.multplication(2, -3), equals(-6));
     });
 
-    test('Multiplying by zero', () {
+    test('Given Calculator class, when multiplication is called and there is a zero, then output zero', () {
       expect(calculatorRepository.multplication(0, 5), equals(0));
     });
 
-    test('Multiplying large numbers', () {
-      expect(calculatorRepository.multplication(1000000000, 2), equals(2000000000));
-    });
-
-    test('Multiplying by one', () {
+    test('Given Calculator class, when multiplication is called and there is a 1, then output same number multiplied to one', () {
       expect(calculatorRepository.multplication(1, 5), equals(5));
     });
 
-    test('Multiplying floating-point numbers', () {
+    test('Given Calculator class, when multiplication is called and there is a decimal number, then output correct product for multiplying floating-point numbers', () {
       expect(calculatorRepository.multplication(2.5, 4), equals(10));
     });
   });
