30836b17be8f2d37021322f37d732ca06d564e72
Jezzer John Fernandez <jfernandez@highleap.com.ph>
2024-02-27 11:54:38 +0800

    group the addtion test

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -8,32 +8,27 @@ setUp(() {
   calculatorRepository = CalculatorRepository();
 });
 
-test('Addition:Given two numbers, When add method is called, Then the sum is calculated correctly', (){
+group('Addition', () { 
+  test('Addition:Given two numbers, When add method is called, Then the sum is calculated correctly', (){
  // Given
   int number1 = 2;
   int number2 = 3;
-
   // When
   int result =  calculatorRepository.addition(number1, number2);
-
   // Then
   expect(result, equals(5));
-
 });
 
 test('Addition: Given two negative numbers, When add method is called, Then the sum is calculated correctly', () {
   // Given
-
   int number1 = -1;
   int number2 = 1;
-
   // When
   int result = calculatorRepository.addition(number1, number2);
-
   // Then
   expect(result, equals(0));
+});});
 
-});
 
 
 
