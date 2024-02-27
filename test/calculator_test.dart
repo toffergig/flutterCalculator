b0541e8cc578c9e3fa7d9f2402fa5cfd247915f0
Kristoffer John Giganto <kristoffersoncool@gmail.com>
2024-02-27 12:00:55 +0800

    clean to calculator test

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -40,6 +40,7 @@ test('Addition: Given two negative numbers, When add method is called, Then the 
   group('Multiplication', () {
     test('Given Calculator class, when multiplication is called and both numbers are positive, then output positive number for multiplying positive numbers', () {
       expect(calculatorRepository.multplication(2, 3), equals(6));
+       expect(calculatorRepository.multplication(3, 2), equals(6));
     });
 
     test('Given Calculator class, when multiplication is called and both numbers are negative, then output positive number for multiplying positive numbers', () {
@@ -48,18 +49,24 @@ test('Addition: Given two negative numbers, When add method is called, Then the 
 
     test('Given Calculator class, when multiplication is called and both are mixed signs, then output negative number for multiplying mixed signed numbers', () {
       expect(calculatorRepository.multplication(2, -3), equals(-6));
+       expect(calculatorRepository.multplication(-3, 2), equals(-6));
     });
 
     test('Given Calculator class, when multiplication is called and there is a zero, then output zero', () {
       expect(calculatorRepository.multplication(0, 5), equals(0));
+      expect(calculatorRepository.multplication(5, 0), equals(0));
+      expect(calculatorRepository.multplication(0, 0), equals(0));
     });
 
     test('Given Calculator class, when multiplication is called and there is a 1, then output same number multiplied to one', () {
       expect(calculatorRepository.multplication(1, 5), equals(5));
+      expect(calculatorRepository.multplication(5, 1), equals(5));
+      expect(calculatorRepository.multplication(1, 1), equals(1));
     });
 
     test('Given Calculator class, when multiplication is called and there is a decimal number, then output correct product for multiplying floating-point numbers', () {
       expect(calculatorRepository.multplication(2.5, 4), equals(10));
+      expect(calculatorRepository.multplication(4, 2.5), equals(10));
     });
   });
 }
