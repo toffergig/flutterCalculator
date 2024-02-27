9652eb040f6c59de999032aef7ff5c14865e4ac4
Medjuil <mailuser0110@gmail.com>
2024-02-27 10:53:11 +0800

    adding calculator repository

--- lib/calculator/data/repository/calculator_repository.dart
+++ lib/calculator/data/repository/calculator_repository.dart
@@ -1,1 +1,25 @@
+class CalculatorRepository {
+  // late double _num1;
+  // late double _num2;
 
+  // CalculatorRepository() {
+  //   _num1 = 0;
+  //   _num2 = 0;
+  // }
+
+  int addition(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  double subtraction(double num1, double num2) {
+    return num1 - num2;
+  }
+
+  double division(double num1, double num2) {
+    return num1 / num2;
+  }
+
+  double multplication(double num1, double num2) {
+    return num1 * num2;
+  }
+}
