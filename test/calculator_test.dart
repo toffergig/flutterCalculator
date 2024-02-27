d9761a8e2e219508be3c4ba6d96365884d581967
Jezzer John Fernandez <jfernandez@highleap.com.ph>
2024-02-27 11:33:48 +0800

    cleaning the code

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -1,18 +1,20 @@
+import 'package:calculator/calculator/data/repository/calculator_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+
 void main() {
-
- 
-
+late CalculatorRepository calculatorRepository;
+setUp(() {
+  calculatorRepository = CalculatorRepository();
+});
 
 test('Addition:Given two numbers, When add method is called, Then the sum is calculated correctly', (){
  // Given
-  final calculator = Calculator();
   int number1 = 2;
   int number2 = 3;
 
   // When
-  int result = calculator.add(number1, number2);
+  int result =  calculatorRepository.addition(number1, number2);
 
   // Then
   expect(result, equals(5));
@@ -21,17 +23,16 @@ test('Addition:Given two numbers, When add method is called, Then the sum is cal
 
 test('Addition: Given two negative numbers, When add method is called, Then the sum is calculated correctly', () {
   // Given
-  final calculator = Calculator();
+
   int number1 = -1;
   int number2 = 1;
 
   // When
-  int result = calculator.add(number1, number2);
+  int result = calculatorRepository.addition(number1, number2);
 
   // Then
   expect(result, equals(0));
 
-  // Additional Then statements for more test cases
 });
 
 
